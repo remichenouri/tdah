c05c7271d145e2347dfc96511d8cfe23dc8e2bd9
remichenouri <remi.chenouri@gmail.com>
2025-06-17 16:30:20 +0200

    Update streamlit_adhd.py

--- streamlit_adhd.py
+++ streamlit_adhd.py
@@ -3090,6 +3090,7 @@ def show_enhanced_ml_analysis():
         from sklearn.naive_bayes import GaussianNB
         from sklearn.model_selection import train_test_split, cross_val_score
         from sklearn.preprocessing import StandardScaler, LabelEncoder
+        from sklearn.impute import SimpleImputer
         from sklearn.metrics import (
             accuracy_score, precision_score, recall_score, f1_score,
             roc_auc_score, confusion_matrix, roc_curve, classification_report
